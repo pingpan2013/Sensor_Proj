e28da56298653a625c4fc1745bfdaabe1a37ceef
pingpan <pingpan@umich.edu>
2014-05-22 15:42:43 -0400

    sensor proj

--- sensor_project/Adafruit_DHT2.py
+++ sensor_project/Adafruit_DHT2.py
@@ -9,10 +9,6 @@ DHT11 = 11
 DHT22 = 22
 AM2302 = 22
 
-#LegacyFlag = 0
-#Legacyt = 0
-#Legacyh = 0
-
 dhtreader.init()
 
 if len(sys.argv) != 3:
@@ -37,23 +33,14 @@ if dhtpin <= 0:
     sys.exit(3)
  
 while True: 
-	try:
-		t, h = dhtreader.read(dev_type, dhtpin)
-		break
-	except TypeError:
-		continue
+    try:
+	t, h = dhtreader.read(dev_type, dhtpin)
+	break
+    except TypeError:
+	continue
 	
 if t and h:
-   print("Temp = {0} *C, Hum = {1} %".format(t, h))
+    print("Temp = {0} *C, Hum = {1} %".format(t, h))
 else:
     print("Failed to read from sensor, maybe try again?")
 
-
-#SerialString = "Temp = {0} *C, Hum = {1} %".format(t, h)
-#if LegacyFlag == 1: SerialString = SerialString + " Leg"
-#print(SerialString)
-#Legacyt = t
-#Legacyh = h
-#LegacyFlag = 0
-
-sleep(2)
