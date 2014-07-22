5f2ef8b6a0aecaba95c0a3bcca2c62d21e62db54
pingpan2013 <pingpan@umich.edu>
2014-07-22 10:46:02 -0400

    gen graphs

--- sensor_project/genGraphs.py
+++ sensor_project/genGraphs.py
@@ -104,37 +104,6 @@ class Gen_Graph:
         plt.draw()
         plt.show()
 
-    def test(self):
-        # To make things reproducible...
-        np.random.seed(1977)
-        fig, ax = plt.subplots()
-       
-        # Twin the x-axis twice to make independent y-axes.
-        axes = [ax, ax.twinx(), ax.twinx()]
-
-        # Make some space on the right side for the extra y-axis.
-        fig.subplots_adjust(right=0.75)
-
-        # Move the last y-axis spine over to the right by 20% of the width of the axes
-        axes[-1].spines['right'].set_position(('axes', 1.2))
-
-        # To make the border of the right-most axis visible, we need to turn the frame
-        # on. This hides the other plots, however, so we need to turn its fill off.
-        axes[-1].set_frame_on(True)
-        axes[-1].patch.set_visible(False)
-
-        # And finally we get to plot things...
-        colors = ('Green', 'Red', 'Blue')
-        for ax, color in zip(axes, colors):
-            data = np.random.random(1) * np.random.random(10)
-            ax.plot(data, marker='o', linestyle='none', color=color)
-            ax.set_ylabel('%s Thing' % color, color=color)
-            ax.tick_params(axis='y', colors=color)
-            axes[0].set_xlabel('X-axis')
-
-        plt.show()
-
-
 def main():
     if len(sys.argv) != 2:
         print "Error with the parameters! Please specify the file path!"
